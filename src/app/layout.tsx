b1a9723e7b5a993cfe78f8fdb4f6c11592c95136
BACOUL <102251510+BACOUL@users.noreply.github.com>
2026-04-13 18:18:25 +0200

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "BoxActions",
+  description: "From message to action",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
